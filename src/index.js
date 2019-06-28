062dbdb535acc8b1fa21d825091104cdd71e5da3
ccan_uw <ccan@uw.edu>
2019-06-28 17:13:35 -0400

    adding basic hrpws support

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import Certificate from './cert';
 import GroupsWebService from './groupsWS';
 import PersonWebService from './personWS';
 import IDCardWebService from './idcardWS';
+import HRPWebService from './hrpWS';
 
 exports.Certificate = Certificate;
 exports.GroupsWebService = GroupsWebService;
 exports.PersonWebService = PersonWebService;
 exports.IDCardWebService = IDCardWebService;
+exports.HRPWebService = HRPWebService;
