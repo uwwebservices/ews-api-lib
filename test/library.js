0ccd79bc3e009810157e0ad338a062c1f7d96ad1
Chris Cannon <ccan@uw.edu>
2019-07-15 16:07:52 -0700

    more tests

--- test/library.js
+++ test/library.js
@@ -19,10 +19,11 @@ const fsIncommonFilePath = path.resolve('config/incommon.pem');
 
 const pwsBaseUrl = 'https://wseval.s.uw.edu/identity/v2';
 const hrpBaseUrl =  'https://wseval.s.uw.edu/hrp/v2';
-const idcardBaseUrl = 'https://wseval.s.uw.edu/idcard/v2';
+const idcardBaseUrl = 'https://wseval.s.uw.edu/idcard/v1';
 const groupsBaseUrl = 'https://groups.uw.edu/group_sws/v3';
 
-const testUser = 'pprestin';
+const testNetids = ['ptaladay', 'pprestin'];
+const testRegid = '899D5562A55911DA84D4E43104487AB3';
 
 let s3cert = null;
 let fscert = null;
@@ -39,8 +40,8 @@ describe('Certificate Tests', function() {
       fscert = certificate.GetPFXFromFS(fsPfxFilePath, fsPassphraseFilePath, fsUWCAFilePath, fsIncommonFilePath);
       pws.Setup(fscert, pwsBaseUrl);
 
-      let resp = await pws.Get(testUser, true);
-      assert.equal(resp.UWNetID, testUser);
+      let resp = await pws.Get(testNetids[0], true);
+      assert.equal(testNetids[0], resp.UWNetID);
     });
   });
   describe('Should Get Files from s3', function() {
@@ -54,8 +55,8 @@ describe('Certificate Tests', function() {
       s3cert = await certificate.GetPFXFromS3(s3Bucket, s3PfxFileName, s3PassphraseFileName, s3UWCAFileName, s3IncommonCert);
       pws.Setup(s3cert, pwsBaseUrl);
 
-      let resp = await pws.Get(testUser, true);
-      assert.equal(resp.UWNetID, testUser);
+      let resp = await pws.Get(testNetids[0], true);
+      assert.equal(testNetids[0], resp.UWNetID);
     });
   });
 });
@@ -74,15 +75,18 @@ describe('Webservice Tests', function () {
   describe('GroupsWS Tests', function() {
     it('Search for test group', async function() {
       let resp = await groups.Search(groupName, 'one');
-      assert.include(resp, groupName);
+      assert.include(groupName, resp);
     });
-    it('Should update group members', async function () {
-      let resp = await groups.UpdateMembers(groupName, ['pprestin'], 'netid');
+    it('Should add group members', async function () {
+      let resp = await groups.UpdateMembers(groupName, [testNetids[0]], 'netid');
       assert.isTrue(resp);
+    });
+    it('Should remove group members', async function () {
+      // not implemented yet
     });
     it('Should get info about a group', async function() {
       let resp = await groups.Info([groupName]);
-      assert.equal(resp[0].id, groupName)
+      assert.equal(groupName, resp[0].id)
     });
     it('Should get group history', async function () {
       let resp = await groups.GetHistory(groupName);
@@ -96,16 +100,39 @@ describe('Webservice Tests', function () {
     })
   });
   
-  describe('HRPWS Tests', async function() {
-    it('', function() {});
+  describe('HRPWS Tests', function() {
+    it('Should get a user from HRPWS', async function() {
+      let resp = await hrp.Get(testNetids[0]);
+      assert.equal(testNetids[0], resp.NetID);
+    });
   });
   
-  describe('IDCardWS Tests', async function() {
-    it('', function() {});
+  describe('IDCardWS Tests', function() {
+    it('Should get a photo by regid', async function() {
+      let resp = await idcard.GetPhoto(testRegid);
+      assert.isNotNull(resp);
+    });
+    it('Should translate magstripe to regid', async function () {
+      // TO DO: need a test magstrip
+    });
+    it('Should translate rfid to regid', async function () {
+      // TO DO: need a test rfid
+    });
   });
   
-  describe('PWS Tests', async function() {
-    it('', function() {});
+  describe('PWS Tests', function() {
+    it('Should get a user', async function() {
+      let resp = await pws.Get(testNetids[0]);
+      assert.equal(testNetids[0], resp.UWNetID);
+    });
+    it('Should get many users', async function() {
+      let resp = await pws.GetMany(testNetids);
+      assert.equal(testNetids.length, resp.length);
+    });
+    it('Should search users', async function() {
+      let resp = await pws.Search(`uwnetid=${testNetids[0]}`);
+      assert.equal(testNetids[0], resp.Persons[0].PersonURI.UWNetID);
+    });
   });
 })
 
